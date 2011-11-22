2d750d01f3b892ab5a6242f569c84165ddc06add
Joan Ang <joanlikecreature@gmail.com>
2011-11-21 22:58:26 -0500

    With more backend outline

---
+++ ui/SYDE461_UI/ExerciseStore.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SYDE461_UI
+{
+    //class for storing all exercise data
+    class ExerciseStore
+    {
+        //THINGS TO ADD:
+        //Read from ExerciseStore
+        //Write to ExerciseStore
+        //Add to ExerciseStore
+        //Sort ExerciseStore
+        //Modify ExerciseStore
+        //Delete entry in ExerciseStore
+
+    }
+}
---
+++ ui/SYDE461_UI/UserInfoStore.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SYDE461_UI
+{
+    // this class is for reading/writing UserInfo to memory
+    // Probably not implementing this until SYDE 462
+    class UserInfoStore
+    {
+        //THINGS TO ADD:
+        //Read from UserInfoStore
+        //Write to UserInfoStore
+        //Add entry to UserInfoStore
+        //Modify entry in UserInfoStore
+        //Sort entries in UserInfoStore
+        //Delete entry from UserInfoStore
+    }
+}
