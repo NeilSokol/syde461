906b01384d94fabb5bcfeb1a616514a08025f127
Neil Sokol <neilsokol@gmail.com>
2011-11-24 02:16:10 -0500

    Tons of changes. Fixed up some UI stuff, added my start of the pinch tracking and measurement image processing, added basic user storage and proper login system

--- ui/SYDE461_UI/ExerciseScreen.Designer.cs
+++ ui/SYDE461_UI/ExerciseScreen.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.fingerDistanceValue = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -36,25 +38,49 @@
             // 
             this.pictureBox1.Location = new System.Drawing.Point(37, 45);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(218, 175);
+            this.pictureBox1.Size = new System.Drawing.Size(357, 222);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            // 
+            // fingerDistanceValue
+            // 
+            this.fingerDistanceValue.AutoSize = true;
+            this.fingerDistanceValue.Location = new System.Drawing.Point(37, 368);
+            this.fingerDistanceValue.Name = "fingerDistanceValue";
+            this.fingerDistanceValue.Size = new System.Drawing.Size(0, 13);
+            this.fingerDistanceValue.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 337);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Finger Distance";
             // 
             // ExerciseScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 266);
+            this.ClientSize = new System.Drawing.Size(435, 426);
+            this.Controls.Add(this.fingerDistanceValue);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "ExerciseScreen";
             this.Text = "Exercise Screen";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ExerciseScreen_FormClosing);
+            this.Load += new System.EventHandler(this.ExerciseScreen_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label fingerDistanceValue;
+        private System.Windows.Forms.Label label1;
     }
 }
