99bf9fef027aff0b02145b360f1e25c942f80726
unknown <Admin@Traveller.(none)>
2011-11-29 21:41:43 -0500

    with Neil's changes

---
+++ ui/SYDE461_UI/Ball.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Data;
+using System.Windows.Shapes;
+using Microsoft.VisualBasic.PowerPacks;
+using System.Windows.Media;
+
+namespace WindowsFormsApplication1
+{
+    public class Ball
+    {
+        //ball standard radius
+        private int ballsize;
+        private Bitmap scene = null;
+        public Graphics g;
+        public int posx = 0;
+        public int posy = 0;
+        public double width = 2;
+        public double height = 2;
+        public Ellipse theBall;
+        public double ballArea = 40;
+        private System.Drawing.Pen bluePen = new System.Drawing.Pen(System.Drawing.Color.Blue, 8);
+
+        public Ball(Bitmap background,double area)
+        {
+            scene = background;
+            ballArea = area;
+            //initialize ball as elipse with size scaled to scene resolution
+            ballsize = (int) Math.Min(scene.VerticalResolution, scene.HorizontalResolution);
+            posx = (int) (scene.HorizontalResolution / 2);
+            posy = (int) (scene.VerticalResolution / 2);
+
+            theBall = new Ellipse();
+        }
+
+
+        public void UpdateBall(int newheight)
+        {
+            height = newheight;
+            width = (ballArea - (0.5 * height)) / Math.PI;
+            g = Graphics.FromImage(scene);
+            g.Clear(System.Drawing.Color.Black);
+            g.DrawEllipse(bluePen, posx, posy, (int)width, (int)height);
+        }
+
+        public void deformBall()
+        {
+            
+
+        }
+
+
+
+    }
+}
--- ui/SYDE461_UI/ExerciseScreen.Designer.cs
+++ ui/SYDE461_UI/ExerciseScreen.Designer.cs
@@ -29,32 +29,71 @@
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.fingerDistanceValue = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ballBox = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ballBox)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.Location = new System.Drawing.Point(37, 45);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(218, 175);
+            this.pictureBox1.Size = new System.Drawing.Size(357, 222);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
+            // 
+            // fingerDistanceValue
+            // 
+            this.fingerDistanceValue.AutoSize = true;
+            this.fingerDistanceValue.Location = new System.Drawing.Point(37, 368);
+            this.fingerDistanceValue.Name = "fingerDistanceValue";
+            this.fingerDistanceValue.Size = new System.Drawing.Size(0, 13);
+            this.fingerDistanceValue.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 337);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Finger Distance";
+            // 
+            // ballBox
+            // 
+            this.ballBox.Location = new System.Drawing.Point(436, 45);
+            this.ballBox.Name = "ballBox";
+            this.ballBox.Size = new System.Drawing.Size(357, 222);
+            this.ballBox.TabIndex = 6;
+            this.ballBox.TabStop = false;
             // 
             // ExerciseScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(292, 266);
+            this.ClientSize = new System.Drawing.Size(850, 431);
+            this.Controls.Add(this.ballBox);
+            this.Controls.Add(this.fingerDistanceValue);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "ExerciseScreen";
             this.Text = "Exercise Screen";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ExerciseScreen_FormClosing);
+            this.Load += new System.EventHandler(this.ExerciseScreen_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ballBox)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label fingerDistanceValue;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox ballBox;
     }
 }
